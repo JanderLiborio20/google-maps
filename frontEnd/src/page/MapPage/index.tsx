948a301047bdbbd928bb2ecacb65eb9a3dee04a4
jander.nunes <jander.nunes@conecthus.org.br>
2024-06-03 18:17:38 -0400

    maker personalid

--- frontEnd/src/page/MapPage/index.tsx
+++ frontEnd/src/page/MapPage/index.tsx
@@ -1,10 +1,13 @@
 import {
   GoogleMap,
+  InfoWindow,
+  Libraries,
   LoadScript,
   Marker,
   StandaloneSearchBox,
 } from '@react-google-maps/api';
 import { useState } from 'react';
+import pin from '../../images/pin.png';
 import { Map } from './styles';
 
 const center = {
@@ -12,29 +15,43 @@ const center = {
   lng: -59.9652881,
 };
 
+const bounds = {
+  north: -2.876,
+  south: -3.241,
+  east: -59.895,
+  west: -60.115,
+};
+
+const libraries: Libraries = ['places'];
+
 const VITE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 export function MapPage() {
   const [map, setMap] = useState<google.maps.Map>();
   const [searchBox, setSearchBox] = useState<google.maps.places.SearchBox>();
-  const [locations, setLocation] = useState<google.maps.LatLngLiteral[]>([]);
+  const [places, setPlaces] = useState<google.maps.places.PlaceResult[]>([]);
+  const [selectedPlace, setSelectedPlace] =
+    useState<google.maps.places.PlaceResult | null>();
 
   const handleOnPlacesChanged = () => {
-    const places = searchBox!.getPlaces();
-    const place = places![0];
+    const searchBoxPlaces = searchBox!.getPlaces();
+    const place = searchBoxPlaces![0];
 
-    const location = {
-      lat: place.geometry?.location?.lat() || 0,
-      lng: place.geometry?.location?.lng() || 0,
-    };
+    setPlaces((currentLocation) => [...currentLocation, place]);
 
-    setLocation((currentLocation) => [...currentLocation, location]);
+    setSelectedPlace(null);
 
-    map?.panTo(location);
+    if (place.geometry && place.geometry.location) {
+      map?.panTo(place.geometry.location);
+    }
   };
 
   return (
-    <LoadScript googleMapsApiKey={VITE_MAPS_API_KEY} libraries={['places']}>
+    <LoadScript
+      googleMapsApiKey={VITE_MAPS_API_KEY}
+      libraries={libraries}
+      region="Amazonas"
+    >
       <Map>
         <GoogleMap
           center={center}
@@ -47,14 +64,38 @@ export function MapPage() {
               <StandaloneSearchBox
                 onLoad={setSearchBox}
                 onPlacesChanged={handleOnPlacesChanged}
+                options={{
+                  bounds: bounds,
+                }}
               >
                 <input type="text" className="search-box-input" />
               </StandaloneSearchBox>
             </div>
           </div>
 
-          {locations.map((location, index) => (
-            <Marker key={index} position={location} />
+          {places.map((place, index) => (
+            <>
+              {place.geometry && place.geometry.location ? (
+                <Marker
+                  key={index}
+                  position={place.geometry.location}
+                  onClick={() => setSelectedPlace(place)}
+                  icon={pin}
+                >
+                  {selectedPlace && selectedPlace === place && (
+                    <InfoWindow
+                      key={`info-window-${index}`}
+                      onCloseClick={() => setSelectedPlace(null)}
+                    >
+                      <div>
+                        <h1>Info Window</h1>
+                        <p>{selectedPlace.formatted_address}</p>
+                      </div>
+                    </InfoWindow>
+                  )}
+                </Marker>
+              ) : null}
+            </>
           ))}
         </GoogleMap>
       </Map>
